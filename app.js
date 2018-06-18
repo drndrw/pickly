b7f03bd0adcb3e7e10f19d35ce23cb84d3b35731
Andrew DaRe <andrew@duh.re>
2018-06-17 23:29:26 -0700

    Added sequelize

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 var express = require('express');
 var app = express();
+require('dotenv').config()
+
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize(process.env.DB_ADDRESS);
 
 app.get('/', (req, res) => {
   res.json({'route': 'index'});
 });
 
 app.listen(3000, () => {
+  sequelize
+.authenticate()
+.then(() => {
+  console.log('Connection has been established successfully.');
+})
+.catch(err => {
+  console.error('Unable to connect to the database:', err);
+});
   console.log('listening');
 })
