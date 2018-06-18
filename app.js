6dae7103c630607d0de1b36500b92917ef0c095c
Andrew DaRe <andrew@duh.re>
2018-06-18 00:04:51 -0700

    Defined 'choice' table, added body-parser

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 var express = require('express');
 var app = express();
-require('dotenv').config()
-
+require('dotenv').config();
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
+// database connection
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.DB_ADDRESS);
 
+const Choice =
+  sequelize.define('choice', {
+    choiceId: {type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true},
+    choiceName: Sequelize.STRING,
+    choiceAddress: Sequelize.STRING,
+    choiceGenre: Sequelize.STRING
+  })
+
 app.get('/', (req, res) => {
   res.json({'route': 'index'});
+});
+
+app.post('/add', jsonParser, (req, res) => {
+  console.log(req.body.test)
+  res.json({'route': 'add'});
 });
 
 app.listen(3000, () => {
@@ -18,5 +33,6 @@ app.listen(3000, () => {
 .catch(err => {
   console.error('Unable to connect to the database:', err);
 });
+  Choice.sync();
   console.log('listening');
 })
