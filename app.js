352b305e07c741dbb975b394ddd77f4a6d5c12cf
Andrew DaRe <andrew@duh.re>
2018-06-18 21:00:02 -0700

    Built user registration endpoint

--- app.js
+++ app.js
@@ -5,13 +5,31 @@ var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json();
 var models = require('./models.js');
 
+// bcrypt config
+var bcrypt = require('bcrypt');
+const saltRounds = parseInt(process.env.BCRYPT_SALT_ROUNDS)
+
 app.get('/', (req, res) => {
   models.testF('testing ya');
-  res.json({'route': 'index'});
+  res.json({'Register': '/user'});
 });
 
 app.post('/add', jsonParser, (req, res) => {
   console.log(req.body.test)
+  res.json({'route': 'add'});
+});
+
+// register user
+app.post('/user', jsonParser, (req, res) => {
+  bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
+    models.User.create({
+      userName: req.body.userName,
+      password: hash,
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email
+    }).then(console.log('User has been created'))
+  });
   res.json({'route': 'add'});
 });
 
