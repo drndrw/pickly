47e62137131257242e093e82144bff5b093f59ff
Andrew DaRe <andrew@duh.re>
2018-07-18 23:03:05 -0700

    Moved models out of modules.exports, added foreign key to choices

--- models.js
+++ models.js
@@ -1,21 +1,44 @@
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.DB_ADDRESS);
 
+const User = sequelize.define('user', {
+    userId: {type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true},
+    userName: {type: Sequelize.STRING, unique: true},
+    password: {type: Sequelize.STRING},
+    firstName: Sequelize.STRING,
+    lastName: Sequelize.STRING,
+    email: {type: Sequelize.STRING, unique: true}
+});
+
+const Choice = sequelize.define('choice', {
+    choiceId: {type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true},
+    choiceCreatorId: Sequelize.INTEGER,
+    choiceName: Sequelize.STRING,
+    choiceAddress: Sequelize.STRING,
+    choiceGenre: Sequelize.STRING
+});
+
+Choice.belongsTo(User, {foreignKey: 'choiceCreatorId'});
+
 module.exports = {
-  User : sequelize.define('user', {
-      userId: {type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true},
-      userName: {type: Sequelize.STRING, unique: true},
-      password: {type: Sequelize.STRING},
-      firstName: Sequelize.STRING,
-      lastName: Sequelize.STRING,
-      email: {type: Sequelize.STRING, unique: true}
-  }),
-  Choice : sequelize.define('choice', {
-      choiceId: {type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true},
-      choiceName: Sequelize.STRING,
-      choiceAddress: Sequelize.STRING,
-      choiceGenre: Sequelize.STRING
-  }),
+  User : User,
+  Choice: Choice,
+  // User : sequelize.define('user', {
+  //     userId: {type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true},
+  //     userName: {type: Sequelize.STRING, unique: true},
+  //     password: {type: Sequelize.STRING},
+  //     firstName: Sequelize.STRING,
+  //     lastName: Sequelize.STRING,
+  //     email: {type: Sequelize.STRING, unique: true}
+  // }),
+  // Choice : sequelize.define('choice', {
+  //     choiceId: {type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true},
+  //     choiceCreatorId: Sequelize.INTEGER,
+  //     choiceName: Sequelize.STRING,
+  //     choiceAddress: Sequelize.STRING,
+  //     choiceGenre: Sequelize.STRING
+  // }),
+  // Choice.belongsTo(User, {foreignKey: 'userName'}),
   dbSync : sequelize.authenticate()
   .then(() => {
     console.log('Connection has been established successfully.');
