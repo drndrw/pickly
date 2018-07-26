759fce8700fdb055de9e6f8cac8176cbc97b10b9
Andrew DaRe <andrew@duh.re>
2018-07-25 18:44:18 -0700

    Moved verifyToken function into middleware.js

---
+++ middleware.js
@@ -0,0 +1,15 @@
+const verifyToken = (req, res, next) => {
+    const jwtToken = req.get('Authorization').split(process.env.JWT_PREFIX + ' ')[1]
+    if (jwtToken) {
+      jwt.verify(jwtToken, process.env.JWT_TOKEN, function(err, decoded) {
+        if (decoded) {
+          req.user = decoded;
+          next();
+        } else {
+          res.json({error: err.message});
+        }
+      });
+    } else {
+      res.json({error: 'Invalid jwt prefix'})
+    }
+};
