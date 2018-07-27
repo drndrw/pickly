f49c4f49ca851ddf934240885171828951f62d83
Andrew DaRe <andrew@duh.re>
2018-07-26 22:32:55 -0700

    Fixed model to work with /category route

--- categories.js
+++ categories.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
-var middleware = require('./middleware.js')
+var middleware = require('./middleware.js');
+var models = require('./models.js');
 
 // show all categories
 router.get('/', middleware.verifyToken, (req, res) => {
-  models.User.findAll({
+  models.Category.findAll({
     attributes: ['categoryId', 'categoryName'],
   }).then(categories => res.json(categories));
 });
