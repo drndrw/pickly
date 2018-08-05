3784ad6c95363049f42b00a91f680bf50fcd03b9
Andrew DaRe <andrew@duh.re>
2018-08-04 17:32:05 -0700

    Add create genre route

--- genres.js
+++ genres.js
@@ -5,11 +5,19 @@ var models = require('./models.js');
 var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json();
 
-// show all categories
+// show all genres
 router.get('/', middleware.verifyToken, (req, res) => {
   models.Genre.findAll({
     attributes: ['genreId', 'genreName'],
   }).then(genres => res.json(genres));
+});
+
+// create a genre
+router.post('/', middleware.verifyToken, jsonParser, (req, res) => {
+  models.Genre.create({
+    genreCreatorId: req.user.user,
+    genreName: req.body.genreName,
+  }).then(genres => res.json({status: 'Created', genreId: genres.get().genreId}).end(), user => res.json({status: 'Error', error: 'Genre name already exists'}));
 });
 
 module.exports = router;
