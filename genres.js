bf8edfaa41a0ae255b98bc1144444ea2e36cbfa0
Andrew DaRe <andrew@duh.re>
2018-08-07 23:02:17 -0700

    Added one-to-many relationships and individual genre route

--- genres.js
+++ genres.js
@@ -5,6 +5,9 @@ var models = require('./models.js');
 var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json();
 
+models.Choice.belongsTo(models.Genre, {foreignKey: 'choiceGenreId', sourceKey: 'choiceId'});
+models.Genre.hasMany(models.Choice, {targetKey: 'choiceId', foreignKey: 'choiceGenreId'});
+
 // show all genres
 router.get('/', middleware.verifyToken, (req, res) => {
   models.Genre.findAll({
@@ -24,6 +27,26 @@ router.post('/', middleware.verifyToken, jsonParser, (req, res) => {
         genreCategoryId: parentCategory.categoryId
       }).then(genres => res.json({status: 'Created', genreId: genres.get().genreId}).end(), user => res.json({status: 'Error', error: 'Genre name already exists'})))
     .catch(parentCategory => res.json({status: 'Error', error: 'Category name invalid or not specified'}))
+});
+
+// view individual genre
+router.get('/:genreId', middleware.verifyToken, jsonParser, (req, res) => {
+  models.Genre.findAll({
+    attributes: ['genreId', 'genreName', 'genreCategoryId'],
+    include: [{
+      model: models.Choice,
+      attributes: ['choiceId', 'choiceName']
+    }],
+    where: {genreId: req.params.genreId}
+  }).then((genre) => {
+    console.log(genre.length)
+    if (genre.length === 0) {
+      res.status(404);
+      res.json({status: 'Error', error: 'Invalid genre ID'});
+    } else {
+      res.json(genre);
+    }
+  })
 });
 
 module.exports = router;
