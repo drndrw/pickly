577c691e402767df61150a7c6f589b27f5f5112e
Andrew DaRe <andrew@duh.re>
2018-09-11 18:29:38 -0700

    Added delete cascade rules to foreign key associations

--- models.js
+++ models.js
@@ -37,15 +37,15 @@ const Genre = sequelize.define('genre', {
 });
 
 User.hasMany(Category, {foreignKey: 'categoryCreatorId', sourceKey: 'userId'});
-Category.belongsTo(User, {foreignKey: 'categoryCreatorId', targetKey: 'userId'});
+Category.belongsTo(User, {foreignKey: 'categoryCreatorId', targetKey: 'userId', onDelete: 'cascade'});
 User.hasMany(Genre, {foreignKey: 'genreCreatorId', sourceKey: 'userId'});
-Genre.belongsTo(User, {foreignKey: 'genreCreatorId', targetKey: 'userId'});
+Genre.belongsTo(User, {foreignKey: 'genreCreatorId', targetKey: 'userId', onDelete: 'cascade'});
 Category.hasMany(Genre, {foreignKey: 'genreCategoryId', sourceKey: 'categoryId'});
-Genre.belongsTo(Category, {foreignKey: 'genreCategoryId', targetKey: 'categoryId'});
+Genre.belongsTo(Category, {foreignKey: 'genreCategoryId', targetKey: 'categoryId', onDelete: 'cascade'});
 User.hasMany(Choice, {foreignKey: 'choiceCreatorId', sourceKey: 'userId'});
-Choice.belongsTo(User, {foreignKey: 'choiceCreatorId', targetKey: 'userId'});
+Choice.belongsTo(User, {foreignKey: 'choiceCreatorId', targetKey: 'userId', onDelete: 'cascade'});
 Genre.hasMany(Choice, {foreignKey: 'choiceGenreId', sourceKey: 'genreId'});
-Choice.belongsTo(Genre, {foreignKey: 'choiceGenreId', targetKey: 'genreId'});
+Choice.belongsTo(Genre, {foreignKey: 'choiceGenreId', targetKey: 'genreId', onDelete: 'cascade'});
 
 module.exports = {
   User: User,
